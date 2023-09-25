22a6a1259de3f90db7e347c76d68da1550fcb7bb
Kaplya <daniklopylov04@icloud.com>
2023-09-25 18:39:41 +0300

    new commit

--- parnerrr.py
+++ parnerrr.py
@@ -1,6 +1,14 @@
-def minus(a, b):
+def mi(a, b):
     return a * b
 
 
+text = 'new'
+
+
+def pul(a, b):
+    return a / b
+
+
 if __name__ == '__main__':
-    print(minus(10, 2))
+    print(mi(10, 2))
+    print(pul(10, 2))
